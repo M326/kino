ecd3d60290342cb4e05e7f0e1504f089481e3f3b
leonard97 <leonard.obernhuber@gmx.ch>
2015-05-14 10:55:12 +0200

    GUI

---
+++ src/mainpanel.java
@@ -0,0 +1,42 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+
+public class mainpanel {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mainpanel window = new mainpanel();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public mainpanel() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 812, 439);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
