ff4d8d1c002bab1b31132ded5982f468af9d4f9a
leonard97 <leonard.obernhuber@gmx.ch>
2015-05-28 10:57:31 +0200

    image_gui

--- src/mainpanel.java
+++ src/mainpanel.java
@@ -1,11 +1,29 @@
 import java.awt.EventQueue;
+import java.awt.Image;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTabbedPane;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import javax.swing.JComboBox;
+import java.awt.FlowLayout;
 
 
 public class mainpanel {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -37,6 +55,121 @@ public class mainpanel {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 812, 439);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.EAST);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_4 = new JPanel();
+		panel.add(panel_4, BorderLayout.NORTH);
+		panel_4.setLayout(new GridLayout(7, 1, 0, 0));
+		
+		JPanel panel_6 = new JPanel();
+		panel_4.add(panel_6);
+		panel_6.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		panel_6.add(lblNewLabel);
+		
+		textField = new JTextField();
+		panel_6.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panel_7 = new JPanel();
+		panel_4.add(panel_7);
+		panel_7.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		panel_7.add(lblNewLabel_1);
+		
+		JSpinner spinner = new JSpinner();
+		panel_7.add(spinner);
+		
+		JPanel panel_8 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_8.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		panel_4.add(panel_8);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		panel_8.add(lblNewLabel_2);
+		
+		JComboBox<?> comboBox = new JComboBox<Object>();
+		panel_8.add(comboBox);
+		
+		JComboBox<?> comboBox_1 = new JComboBox<Object>();
+		panel_8.add(comboBox_1);
+		
+		JPanel panel_9 = new JPanel();
+		FlowLayout flowLayout_1 = (FlowLayout) panel_9.getLayout();
+		flowLayout_1.setAlignment(FlowLayout.LEFT);
+		panel_4.add(panel_9);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setMaximumRowCount(20);
+		panel_9.add(comboBox_2);
+		
+		JPanel panel_10 = new JPanel();
+		panel_4.add(panel_10);
+		
+		textField_1 = new JTextField();
+		panel_10.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		panel_10.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JPanel panel_11 = new JPanel();
+		panel_4.add(panel_11);
+		
+		textField_3 = new JTextField();
+		panel_11.add(textField_3);
+		textField_3.setColumns(22);
+		
+		JPanel panel_12 = new JPanel();
+		panel_4.add(panel_12);
+		
+		textField_4 = new JTextField();
+		textField_4.setEnabled(false);
+		panel_12.add(textField_4);
+		textField_4.setColumns(22);
+		
+		JPanel panel_5 = new JPanel();
+		panel.add(panel_5, BorderLayout.SOUTH);
+		panel_5.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JButton btnNewButton = new JButton("New button");
+		panel_5.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		panel_5.add(btnNewButton_1);
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		panel_1.add(tabbedPane, BorderLayout.NORTH);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_2, null);
+		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblNewLabel_3 = new JLabel("");
+		panel_2.add(lblNewLabel_3);
+		
+		Image img = new ImageIcon(this.getClass().getResource("/kinosaal.jpg")).getImage();
+		lblNewLabel_3.setIcon(new ImageIcon(img));
+		
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		panel_1.add(tabbedPane_1, BorderLayout.SOUTH);
+		
+		JPanel panel_3 = new JPanel();
+		tabbedPane_1.addTab("New tab", null, panel_3, null);
+		
+		
+		
 	}
 
 }
